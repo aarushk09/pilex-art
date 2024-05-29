79cb7b5ae9e60c578b668192b417270dd50ba539
Aarush Kute <145231915+aarushk09@users.noreply.github.com>
2024-05-28 23:09:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -49,9 +49,9 @@ sizeEl.addEventListener('keyup', function () {
 
 container.addEventListener('wheel', function (event) {
     event.preventDefault();
-    if (event.deltaY < 0 && size <= standardSize) {
+    if (event.deltaY > 0 && size <= standardSize) {
         size += 5; // Increase grid size
-    } else if (event.deltaY > 0) {
+    } else if (event.deltaY < 0) {
         size -= 5; // Decrease grid size
         size = Math.max(1, size); // Ensure size doesn't go below 1
     }
